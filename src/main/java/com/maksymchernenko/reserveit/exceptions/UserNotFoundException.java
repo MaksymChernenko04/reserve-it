335f6e7c9d5e590d842b4c0a32274b4108033860
Maksym <chernenkomax04@gmail.com>
2025-07-15 14:07:33 +0200

    Add simple errors handling for login and registration

--- src/main/java/com/maksymchernenko/reserveit/exceptions/UserNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.maksymchernenko.reserveit.exceptions;
-
-public class UserNotFoundException extends Exception {
-    public UserNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/maksymchernenko/reserveit/exceptions/WrongPasswordException.java
+++
@@ -1,7 +0,0 @@
-package com.maksymchernenko.reserveit.exceptions;
-
-public class WrongPasswordException extends Exception {
-    public WrongPasswordException(String message) {
-        super(message);
-    }
-}
