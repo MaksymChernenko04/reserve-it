d507528858db7783dd16eb565ffee4a2caadba06
Maksym <chernenkomax04@gmail.com>
2025-08-31 18:19:06 +0200

    Add Javadoc to the class that contains a main method

--- src/main/java/com/maksymchernenko/reserveit/ReserveItApplication.java
+++ src/main/java/com/maksymchernenko/reserveit/ReserveItApplication.java
@@ -3,11 +3,18 @@ package com.maksymchernenko.reserveit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * ReserveIt application.
+ */
 @SpringBootApplication
 public class ReserveItApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input command-line arguments
+     */
     public static void main(String[] args) {
         SpringApplication.run(ReserveItApplication.class, args);
     }
-
 }
